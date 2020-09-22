8e68c6398904953ed73ffedbc9ed269b0cbe368f
Igor Lisogor <illisogori@gmail.com>
2020-09-22 23:26:12 +0300

    Loading screen

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpiner () {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+    if(!loader.hidden) {
+        loader.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
+
 // Get Quote From API
 async function getQuote() {
+    showLoadingSpiner ();
     const proxyURL = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyURL + apiUrl);
         const data = await response.json();
-        console.log(data);
+        if(data.authorText === ''){
+            authorText.innerText = 'Umknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        if(data.quoteText > 80){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //console.log(data);
     } catch (error) {
-        console.log('no quote', error);
+        getQuote();
+        //console.log(error);
     }
+    hideLoadingSpinner();
 }
 
+//  Tweet Quote 
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 //On Load
 getQuote();
